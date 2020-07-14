7cfaae49d99241a00520bfd18223dadfa587c4a5
alvin <@>
2020-07-13 23:26:16 -0700

    remove logger in prod

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,7 +3,7 @@ import logger from 'redux-logger';
 import finalReducer from './reducers';
 import thunk from "redux-thunk";
 
-const middleware = applyMiddleware(logger,thunk); // remove logger in production
-// const middleware = applyMiddleware(thunk);
+// const middleware = applyMiddleware(logger,thunk); // remove logger in production
+const middleware = applyMiddleware(thunk);
 
 export default createStore(finalReducer, middleware);
